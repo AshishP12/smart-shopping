c5bf2ae0d03d85c339c0664191305993560ed0af
SmitaSrivastava09 <78153542+SmitaSrivastava09@users.noreply.github.com>
2021-03-23 01:19:06 +0530

    created the required classes for Address, User and Product and tested it with Junit

---
+++ UserDAO.java
@@ -0,0 +1,26 @@
+package net.spvra.shoppingbackend.dao;
+
+import java.util.List;
+
+import net.spvra.shoppingbackend.dto.Address;
+import net.spvra.shoppingbackend.dto.Cart;
+import net.spvra.shoppingbackend.dto.User;
+
+public interface UserDAO {
+
+	// user related operation
+	User getByEmail(String email);
+	User get(int id);
+
+	boolean add(User user);
+	
+	// adding and updating a new address
+	Address getAddress(int addressId);
+	boolean addAddress(Address address);
+	boolean updateAddress(Address address);
+	Address getBillingAddress(User user);
+	List<Address> listShippingAddresses(User user);
+	
+
+	
+}
